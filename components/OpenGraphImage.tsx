5831a40468b8063db35e7a168c3ad3d9e6597549
FluxCapacitor2 <31071265+FluxCapacitor2@users.noreply.github.com>
2023-01-25 14:51:01 -0500

    Make OG images larger in cards/previews

--- components/OpenGraphImage.tsx
+++ components/OpenGraphImage.tsx
@@ -1,7 +1,10 @@
 import { BASE_PATH } from "@/app/vars";
 
-export default function OpenGraphImage({ relative }: { relative: string}) {
+export default function OpenGraphImage({ relative }: { relative: string }) {
     return (
-        <meta name="og:image" key="image" content={BASE_PATH + relative} />
+        <>
+            <meta name="og:image" key="image" content={BASE_PATH + relative} />
+            {relative.includes("/api/og") ? <meta name="twitter:card" content="summary_large_image" /> : <></>}
+        </>
     )
 }
