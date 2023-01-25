65b65956917bb51939da4c4b7b5aec3388ef72bc
FluxCapacitor2 <31071265+FluxCapacitor2@users.noreply.github.com>
2023-01-24 23:23:13 -0500

    Fix og:description meta on leaderboard pages

--- app/head.tsx
+++ app/head.tsx
@@ -6,11 +6,11 @@ export default function Head() {
     <>
       <BaseHead image="/api/og" />
 
-      <title key="page-title">BlueDragon</title>
+      <title>BlueDragon</title>
 
-      <meta name="og:title" key="title" content="BlueDragon | Minecraft Minigames" />
-      <meta name="og:description" key="description" content="BlueDragon is an original Minecraft server featuring minigames like WackyMaze, PvPMaster, FastFall, and many more!" />
-      <meta name="og:type" key="type" content="website" />
+      <meta name="og:title" content="BlueDragon | Minecraft Minigames" />
+      <meta name="og:description" content="BlueDragon is an original Minecraft server featuring minigames like WackyMaze, PvPMaster, FastFall, and many more!" />
+      <meta name="og:type" content="website" />
     </>
   )
 }
--- app/leaderboards/[stat]/head.tsx
+++ app/leaderboards/[stat]/head.tsx
@@ -23,5 +23,6 @@ export default function Head({ params: { stat } }: { params: { stat: string } })
         <BaseHead image={`/api/og?title=${encodeURIComponent(cat.name + ": " + lb.name)}&ogPreview=${encodeURIComponent("View the top 10 players for this leaderboard on bluedragonmc.com, or join the server and climb the ranks yourself.")}`} />
         <meta name="og:type" content="article" />
         <meta name="og:title" content={`${cat.name}: ${lb.name} | BlueDragon`} />
+        <meta name="og:description" content={`View the top 10 players for the ${cat.name} ${lb.name} leaderboard online, or join the server and climb the ranks yourself.`} />
     </>
 }
