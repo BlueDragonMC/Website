7eecefe4710a54d635712b18476f68a412b8348b
FluxCapacitor2 <31071265+FluxCapacitor2@users.noreply.github.com>
2023-02-05 01:52:47 -0500

    Redesign footer

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,4 +1,3 @@
-import Image from "next/image";
 import Link from "next/link";
 import { Discord, GitHub, Twitter } from "./Icons";
 
@@ -6,11 +5,10 @@ export default function Footer() {
     return (
         <footer
             className={
-                "min-h-64 relative z-40 mt-36 grid grid-cols-1 grid-rows-2 items-center bg-neutral-300 p-4 dark:bg-gray-700 md:grid-cols-2 lg:grid-cols-3"
+                "min-h-64 relative z-40 mt-36 grid grid-cols-2 gap-4 bg-neutral-300 px-4 py-8 dark:bg-neutral-900 md:grid-cols-3 md:gap-8 lg:grid-cols-6"
             }
         >
-            {/* First row */}
-            <div>
+            <div className="col-span-2 md:col-span-3 lg:col-span-2">
                 <h1 className={"text-2xl font-bold"}>BlueDragon</h1>
                 <p>
                     BlueDragon is an original Minecraft server featuring
@@ -24,44 +22,53 @@ export default function Footer() {
                     </Link>
                 </p>
             </div>
-            <div className="hidden justify-end md:flex lg:col-span-2">
-                <Image
-                    src="/favicon_hq.png"
-                    height={128}
-                    width={128}
-                    alt="BlueDragon Logo"
-                ></Image>
-            </div>
             {/* Second row */}
-            <div>
-                <Link href="https://discord.gg/3gvSPdW" className="mr-5">
-                    <Discord className="inline h-8 fill-black dark:fill-white" />
+            <div className="flex flex-col leading-7 lg:col-start-4">
+                <p className="font-bold">Server</p>
+                <Link href="/games" className="font-medium underline">
+                    Games
                 </Link>
-                <Link href="https://github.com/BlueDragonMC" className="mr-5">
-                    <GitHub className="inline h-8 fill-black dark:fill-white" />
+                <Link href="/about" className="font-medium underline">
+                    About
                 </Link>
-                <Link href="https://twitter.com/BDMCNetwork">
-                    <Twitter className="inline h-8 fill-black dark:fill-white" />
+                <Link href="/leaderboards" className="font-medium underline">
+                    Leaderboards
+                </Link>
+                <Link href="/player" className="font-medium underline">
+                    Player Stats
                 </Link>
             </div>
-            <div className="md:text-right lg:col-span-2">
-                <span>&copy; 2023 BlueDragonMC</span>
-                <br />
+            <div className="flex flex-col leading-7">
+                <p className="font-bold">Community</p>
                 <Link href="/page/changelog" className="font-medium underline">
                     Changelog
-                </Link>{" "}
-                &middot;{" "}
+                </Link>
                 <Link href="/page/rules" className="font-medium underline">
                     Server Rules
-                </Link>{" "}
-                &middot;{" "}
-                <Link href="/page/oss" className="font-medium underline">
-                    Open&#8209;Source&nbsp;Software
-                </Link>{" "}
-                &middot;{" "}
+                </Link>
                 <Link href="/status" className="font-medium underline">
                     Server&nbsp;Status
                 </Link>
+                <Link href="/page/oss" className="font-medium underline">
+                    Open&#8209;Source&nbsp;Software
+                </Link>
+            </div>
+            <div className="col-span-1 flex flex-col sm:col-span-2 md:col-span-1">
+                <p className="mb-2 font-bold">Follow Us</p>
+                <div>
+                    <Link
+                        href="https://github.com/BlueDragonMC"
+                        className="mr-5"
+                    >
+                        <GitHub className="inline h-8 fill-black transition-colors hover:fill-[#181717] dark:fill-white" />
+                    </Link>
+                    <Link href="https://discord.gg/3gvSPdW" className="mr-5">
+                        <Discord className="inline h-8 fill-black transition-colors hover:fill-[#5865F2] dark:fill-white" />
+                    </Link>
+                    <Link href="https://twitter.com/BDMCNetwork">
+                        <Twitter className="inline h-8 fill-black transition-colors hover:fill-[#1DA1F2] dark:fill-white" />
+                    </Link>
+                </div>
             </div>
         </footer>
     );
