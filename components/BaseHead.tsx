c62d682f160e1f7097ce726cabcd48c1787f6668
FluxCapacitor2 <31071265+FluxCapacitor2@users.noreply.github.com>
2023-02-07 18:42:10 -0500

    Add more favicons

--- components/BaseHead.tsx
+++ components/BaseHead.tsx
@@ -12,7 +12,39 @@ export default function BaseHead({ image }: { image?: string }) {
                 name="description"
                 content="BlueDragon is an original Minecraft server featuring minigames like WackyMaze, PvPMaster, FastFall, and many more!"
             />
-            <link rel="icon" href="/favicon_hq.png" />
+            {/* Favicons */}
+            <link
+                rel="icon"
+                type="image/png"
+                href="favicon-196x196.png"
+                sizes="196x196"
+            />
+            <link
+                rel="icon"
+                type="image/png"
+                href="favicon-96x96.png"
+                sizes="96x96"
+            />
+            <link
+                rel="icon"
+                type="image/png"
+                href="favicon-32x32.png"
+                sizes="32x32"
+            />
+            <link
+                rel="icon"
+                type="image/png"
+                href="favicon-16x16.png"
+                sizes="16x16"
+            />
+            <link
+                rel="icon"
+                type="image/png"
+                href="favicon-128.png"
+                sizes="128x128"
+            />
+            <meta name="application-name" content="BlueDragon" />
+
             <OpenGraphImage relative={image ?? "/api/og"} />
             <OpenGraphURL />
         </>
