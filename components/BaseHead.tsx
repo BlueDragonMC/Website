8f55335ddd30c555eb90c71b2b52670c6ddd731c
FluxCapacitor2 <31071265+FluxCapacitor2@users.noreply.github.com>
2023-02-08 22:36:17 -0500

    Fix favicons

--- components/BaseHead.tsx
+++ components/BaseHead.tsx
@@ -16,31 +16,31 @@ export default function BaseHead({ image }: { image?: string }) {
             <link
                 rel="icon"
                 type="image/png"
-                href="favicon-196x196.png"
+                href="/favicon-196x196.png"
                 sizes="196x196"
             />
             <link
                 rel="icon"
                 type="image/png"
-                href="favicon-96x96.png"
+                href="/favicon-96x96.png"
                 sizes="96x96"
             />
             <link
                 rel="icon"
                 type="image/png"
-                href="favicon-32x32.png"
+                href="/favicon-32x32.png"
                 sizes="32x32"
             />
             <link
                 rel="icon"
                 type="image/png"
-                href="favicon-16x16.png"
+                href="/favicon-16x16.png"
                 sizes="16x16"
             />
             <link
                 rel="icon"
                 type="image/png"
-                href="favicon-128.png"
+                href="/favicon-128.png"
                 sizes="128x128"
             />
             <meta name="application-name" content="BlueDragon" />
--- pages/api/og.tsx
+++ pages/api/og.tsx
@@ -1,3 +1,10 @@
+import FontAwesomeIcon from "@/components/FontAwesomeIcon";
+import {
+    faBookmark,
+    faCalendar,
+    faClock,
+    faUser,
+} from "@fortawesome/free-solid-svg-icons";
 import { ImageResponse } from "@vercel/og";
 import { NextRequest } from "next/server";
 
@@ -11,53 +18,6 @@ const favicon = new URL(
     "../../public/favicon_hq.png",
     import.meta.url
 ).toString();
-
-// Icons provided by Font Awesome
-// https://fontawesome.com/
-
-const userIcon = (
-    <svg
-        xmlns="http://www.w3.org/2000/svg"
-        viewBox="0 0 448 512"
-        fill="#9ca3af"
-        height={32}
-    >
-        <path d="M272 304h-96C78.8 304 0 382.8 0 480c0 17.67 14.33 32 32 32h384c17.67 0 32-14.33 32-32C448 382.8 369.2 304 272 304zM48.99 464C56.89 400.9 110.8 352 176 352h96c65.16 0 119.1 48.95 127 112H48.99zM224 256c70.69 0 128-57.31 128-128c0-70.69-57.31-128-128-128S96 57.31 96 128C96 198.7 153.3 256 224 256zM224 48c44.11 0 80 35.89 80 80c0 44.11-35.89 80-80 80S144 172.1 144 128C144 83.89 179.9 48 224 48z" />
-    </svg>
-);
-
-const calendarIcon = (
-    <svg
-        xmlns="http://www.w3.org/2000/svg"
-        viewBox="0 0 448 512"
-        fill="#9ca3af"
-        height={32}
-    >
-        <path d="M152 64H296V24C296 10.75 306.7 0 320 0C333.3 0 344 10.75 344 24V64H384C419.3 64 448 92.65 448 128V448C448 483.3 419.3 512 384 512H64C28.65 512 0 483.3 0 448V128C0 92.65 28.65 64 64 64H104V24C104 10.75 114.7 0 128 0C141.3 0 152 10.75 152 24V64zM48 448C48 456.8 55.16 464 64 464H384C392.8 464 400 456.8 400 448V192H48V448z" />
-    </svg>
-);
-
-const clockIcon = (
-    <svg
-        xmlns="http://www.w3.org/2000/svg"
-        viewBox="0 0 512 512"
-        fill="#9ca3af"
-        height={32}
-    >
-        <path d="M232 120C232 106.7 242.7 96 256 96C269.3 96 280 106.7 280 120V243.2L365.3 300C376.3 307.4 379.3 322.3 371.1 333.3C364.6 344.3 349.7 347.3 338.7 339.1L242.7 275.1C236 271.5 232 264 232 255.1L232 120zM256 0C397.4 0 512 114.6 512 256C512 397.4 397.4 512 256 512C114.6 512 0 397.4 0 256C0 114.6 114.6 0 256 0zM48 256C48 370.9 141.1 464 256 464C370.9 464 464 370.9 464 256C464 141.1 370.9 48 256 48C141.1 48 48 141.1 48 256z" />
-    </svg>
-);
-
-const bookmarkIcon = (
-    <svg
-        xmlns="http://www.w3.org/2000/svg"
-        viewBox="0 0 384 512"
-        fill="#9ca3af"
-        height={32}
-    >
-        <path d="M336 0h-288C21.49 0 0 21.49 0 48v431.9c0 24.7 26.79 40.08 48.12 27.64L192 423.6l143.9 83.93C357.2 519.1 384 504.6 384 479.9V48C384 21.49 362.5 0 336 0zM336 452L192 368l-144 84V54C48 50.63 50.63 48 53.1 48h276C333.4 48 336 50.63 336 54V452z" />
-    </svg>
-);
 
 export const config = {
     runtime: "edge",
@@ -173,22 +133,38 @@ export default async function OpenGraphImage(req: NextRequest) {
                     tw="text-gray-400"
                 >
                     <div style={{ display: author ? "flex" : "none" }}>
-                        {userIcon}
+                        <FontAwesomeIcon
+                            icon={faUser}
+                            fill="#9ca3af"
+                            height={32}
+                        />
                         <span tw="ml-2">{author}</span>
                     </div>
 
                     <div style={{ display: date ? "flex" : "none" }}>
-                        {calendarIcon}
+                        <FontAwesomeIcon
+                            icon={faCalendar}
+                            fill="#9ca3af"
+                            height={32}
+                        />
                         <span tw="ml-2">{date}</span>
                     </div>
 
                     <div style={{ display: readTime ? "flex" : "none" }}>
-                        {clockIcon}
+                        <FontAwesomeIcon
+                            icon={faClock}
+                            fill="#9ca3af"
+                            height={32}
+                        />
                         <span tw="ml-2">{readTime}</span>
                     </div>
 
                     <div style={{ display: "flex" }}>
-                        {bookmarkIcon}
+                        <FontAwesomeIcon
+                            icon={faBookmark}
+                            fill="#9ca3af"
+                            height={32}
+                        />
                         <span tw="ml-2">bluedragonmc.com</span>
                     </div>
                 </div>
